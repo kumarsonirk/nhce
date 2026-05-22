2a6a822438d17b1ad367c5ab0b9569187114bca2
kumarsonirk <rkrahul16716@gmail.com>
2026-05-22 14:39:38 +0530

    video fix

--- src/components/sections/VideoSection.tsx
+++ src/components/sections/VideoSection.tsx
@@ -1,14 +1,13 @@
 export default function VideoSection() {
   return (
     <section className="bg-navy-950">
-      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+      <div className="relative w-full overflow-hidden" style={{ paddingBottom: '56.25%' }}>
         <iframe
-          src="https://www.youtube.com/embed/uWSISyKjpQs?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&disablekb=1&iv_load_policy=3&loop=1&playlist=uWSISyKjpQs"
+          src="https://www.youtube.com/embed/uWSISyKjpQs?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&disablekb=1&iv_load_policy=3&loop=1&playlist=uWSISyKjpQs&showinfo=0&fs=0"
           title="NHCE Campus Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-          className="absolute inset-0 w-full h-full"
-          style={{ border: 'none' }}
+          className="absolute w-[130%] h-[130%] -top-[15%] -left-[15%]"
+          style={{ border: 'none', pointerEvents: 'none' }}
         />
       </div>
     </section>
